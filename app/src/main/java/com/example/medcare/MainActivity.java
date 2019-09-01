78288d3ebbfe2994dd898dec6340a4739125e030
saurabhfit <saurabhfit@gmail.com>
2019-09-01 23:02:48 +0530

    Saurabh commit1

--- app/src/main/java/com/example/medcare/MainActivity.java
+++ app/src/main/java/com/example/medcare/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.medcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static int SPLASH_TIMEOUT = 10000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+//                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+//                startActivity(loginIntent);
+                finish();
+            }
+        }, SPLASH_TIMEOUT);
     }
 }
